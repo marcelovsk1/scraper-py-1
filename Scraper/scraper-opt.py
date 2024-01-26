a252f0ad5971d69357f7d528d3d63660efa0c42c
Marcelo Amaral Alves <marceloaa@live.com>
2024-01-26 15:12:08 -0300

    working on

--- Scraper/scraper-opt.py
+++ Scraper/scraper-opt.py
@@ -64,7 +64,7 @@ def main():
             'selectors': {
                 'event': {'tag': 'div', 'class': 'Stack_root__1ksk7'}, # is working well
                 'Event': {'tag': 'h2', 'class': 'Typography_root__487rx #3a3247 Typography_body-lg__487rx event-card__clamp-line--two Typography_align-match-parent__487rx'}, # is working well
-                'Date': {'tag': 'p', 'class': 'Typography_root__487rx #3a3247 Typography_body-md-bold__487rx Typography_align-match-parent__487rx'},
+                'Date': {'tag': 'p', 'class': 'Typography_root__487rx #3a3247 Typography_body-md-bold__487rx Typography_align-match-parent__487rx'}, # is working well
                 'Location': {'tag': 'p', 'class': 'Typography_root__487rx #585163 Typography_body-md__487rx event-card__clamp-line--one Typography_align-match-parent__487rx'},
                 'Image URL': {'tag': 'img', 'class': 'event-card-image'} # try to find a solution for the IMG
             }
